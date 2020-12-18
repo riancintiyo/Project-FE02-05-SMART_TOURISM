9f9f5a747c0345341db58f7a54855b0d6038f830
rionse09 <44936981+riancintiyo@users.noreply.github.com>
2020-12-18 21:00:02 +0700

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as HeaderPage } from './headerPage'
+export { default as Hero } from './hero'
+export { default as DatePickers } from './bookBox'
+export { default as Footer} from './footer'
